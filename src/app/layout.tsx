c3a57f3dd316ed36bb040cc9658b882d266ab3ba
Achmad Hendarsyah <ahmdhndrsyh@gmail.com>
2026-01-31 11:08:49 +0700

    chore: update setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,14 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="font-jakarta bg-primary-100 h-full w-full text-neutral-100 antialiased">
+      <body className="font-jakarta bg-primary-200 h-full w-full text-neutral-100 antialiased">
         <div className="flex h-screen w-screen flex-col justify-between">
-          <main className="container flex-grow">{children}</main>
+          <main className="container grow">{children}</main>
           <footer className="border-secondary-100 flex h-14 items-center justify-center border-t font-sans">
             &copy; {new Date().getFullYear()} By{" "}
             <span className="ml-1">
               <Link href={"https://ahmdhndr.vercel.app/"} target="_blank">
-                <Button variant={"link"} className="text-secondary-150 p-0">
+                <Button variant={"link"} className="text-secondary-200 p-0">
                   Achmad Hendarsyah
                 </Button>
               </Link>
